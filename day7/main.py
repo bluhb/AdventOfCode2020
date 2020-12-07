2119e02ccc83aae7e059431cf369ebbf89958dda
Bluhb_ <rogiervtol@gmail.com>
2020-12-07 13:59:22 +0100

    add better regex function

--- day7/main.py
+++ day7/main.py
@@ -1,42 +1,36 @@
 import FileRead
 import re
 f = FileRead.ReadInput("input.txt")
-re_start = re.compile("^(.+) bags contain (?:(?:(\d) (.+) bag(?:s?|s?\.))|no other bags\.)")
-re_rest = re.compile("(?:(\d) (.+) bag(?:s?))")
+
+re_search = re.compile("(?:(.+) bags contain)? (?:(?:(\d) (.+?) bags?)|no other bags)")
 
 def parseToRuleDict(r):
-    parts = r.split(",")
-    contain = []
-    startgroups = re.search(re_start, parts[0])
-    contain.extend(startgroups.groups())
-    for i in parts[1:]:
-        groups = re.search(re_rest, i)
-        if groups is None:
-            print(i)
-        else:
-            contain.extend(groups.groups())
-    rule_dict = {contain[0]: contain[1:]}
-    return rule_dict
+    groups = re.findall(re_search, r)
+    groups = [y for tuples in groups for y in tuples if y != ""]
+    if len(groups) > 0:
+        rule_dict = {groups[0]: groups[1:]}
+        return rule_dict
+    return {}
 
 def searchLuggageBag(bags, my_bag, current_bag):
-    if current_bag == my_bag:
+    if current_bag == my_bag: #returns when my_bag is found in another bag
         return 1
-    elif bags.get(current_bag) is None or bags.get(current_bag)[0] is None:
+    elif bags.get(current_bag) is None or len(bags.get(current_bag)) == 0: #if there are no bags inside this bag
         return 0
     else:
         counts = []
-        for k in bags[current_bag]:
+        for k in bags[current_bag]: #check for every bag in the current bag
             counts.append(searchLuggageBag(bags, my_bag, k))
-        return (max(counts))
+        return (max(counts)) #return 0 or 1 depending on if my bag is found
 
 def countNeededBags(rules, my_bag):
-    if my_bag is None or rules[my_bag][0] is None:
-        return 1
+    if my_bag is None or len(rules[my_bag]) == 0:
+        return 1 #only one bag is in here
     contents = rules[my_bag]
-    total = [1]
+    total = []
     for i in range(0, len(contents), 2):
         total.append(int(contents[i]) * countNeededBags(rules, contents[i+1]))
-    return sum(total)
+    return sum(total) + 1 #plus one for the bag that you are checking
 
 
 def solution1():
@@ -46,7 +40,7 @@ def solution1():
     for i in f:
         rules.update(parseToRuleDict(i))
     for k,v in rules.items():
-        if k != my_bag:
+        if k != my_bag: #don't count my own bag
             amount += (searchLuggageBag(rules, my_bag, k))
     print(amount)
 
