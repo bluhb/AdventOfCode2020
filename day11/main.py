36e5e3bca11883593f7410e0b603b01586bbe9c1
Bluhb_ <rogiervtol@gmail.com>
2020-12-11 12:29:10 +0100

    day 11 done

--- day11/main.py
+++ day11/main.py
@@ -2,7 +2,7 @@ import FileRead
 
 FILENAME = "input.txt"
 
-def checkSeat1(data, row, column):
+def checkSeat1(data, row, column): #check neighbours
     current = data[row][column]
     change = True
     if current == "L":
@@ -32,6 +32,34 @@ def checkSeat1(data, row, column):
                     return True
         return False
 
+#check line of sight
+def checkSeat2(current, direction, data):
+    """
+    current = [row, column] coordinates
+    direction = lu(left up) u(up) ru(right up) l(left) r(right) ld(left down) d(down) rd(right down)
+    data = the seats
+    """
+    directions = ["u", "ru", "r", "rd", "d", "ld", "l", "lu"]
+
+    #add direction to current position
+    current[0] += -1 if "u" in direction else 0
+    current[0] += 1 if "d" in direction else 0
+    current[1] += 1 if "r" in direction else 0
+    current[1] += -1 if "l" in direction else 0
+
+    #check if new current is still in the raster
+    if not (0 <= current[0] < len(data) and 0 <= current[1] < len(data[0])):
+        return 0
+    elif  data[current[0]][current[1]] == "#": #if occupied return 1
+        return 1
+    elif data[current[0]][current[1]] == "L": #if empty seat, return 0
+        return 0
+    elif data[current[0]][current[1]] == ".": #if no seat, call function again to check next position
+        if direction in directions:
+            return checkSeat2(current, direction, data)
+        else:
+            ValueError("unknown direction")
+
 def solution1():
     f = FileRead.ReadInput(FILENAME)
     for row in range(0,len(f)):
@@ -52,14 +80,48 @@ def solution1():
     count = sum([x.count("#") for x in seats])
     return [count]
 
+def solution2():
+    f = FileRead.ReadInput(FILENAME)
+    directions = ["u", "ru", "r", "rd", "d", "ld", "l", "lu"]
+    data = [list(x) for x in f]
+    working = []
+
+    while working != data:
+        working = [x[:] for x in data]
+        for row in range(0,len(data)):
+            for column in range(0,len(data[0])):
+                occupiedSeats = 0
+
+                if working[row][column] == "#":
+                    for direction in directions:
+                        occupiedSeats += checkSeat2([row, column], direction, working)
+                        if occupiedSeats >= 5:
+                            data[row][column] = "L"
+                            break
+
+                elif working[row][column] == "L":
+                    for direction in directions:
+                        occupiedSeats += checkSeat2([row, column], direction, working)
+                        if occupiedSeats > 0:
+                            break
+                    if occupiedSeats == 0:
+                        data[row][column] = "#"
+
+    return [sum([x.count("#") for x in data])]
+
+
 
 def timeIt(func):
     import time
+    print("Running {}".format(func.__name__))
     t1 =time.perf_counter()
     answer = func()
     t1 = time.perf_counter() - t1
     print("Time taken by {}: {:.5f}s".format(func.__name__, t1))
     print(*answer, sep="\n")
+    print("")
     return t1
 
-timeIt(solution1)
+t1 = timeIt(solution1)
+t2 = timeIt(solution2)
+print("Total time is {}s".format(t1 + t2))
